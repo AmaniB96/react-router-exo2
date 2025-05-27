3aba0889f40a881d3ca50899a6d5ffa73ef3409b
amani <bataganiramani@gmail.com>
2025-05-27 11:07:16 +0200

    nav done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,8 +7,17 @@ export default function Nav() {
     return (
         <>
         <nav>
-            <div>
+            <div className='logo-container'>
                 <img src={Logo} alt="Logo" />
+            </div>
+
+            <div className='navigation-container'>
+                <ul>
+                    <li>00 HOME</li>
+                    <li>01 DESTINATION</li>
+                    <li>02 CREW</li>
+                    <li>03 TECHNOLOGY</li>
+                </ul>
             </div>
         </nav>
         </>

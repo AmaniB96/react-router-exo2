d0d3fccc66496694c86fa123b45024f6bfa1db8f
amani <bataganiramani@gmail.com>
2025-05-27 10:11:37 +0200

    nav creation

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import './nav.css'
+import Logo from '../../public/assets/shared/logo.svg'
+
+export default function Nav() {
+    
+
+    return (
+        <>
+        <nav>
+            <div>
+                <img src={Logo} alt="Logo" />
+            </div>
+        </nav>
+        </>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Nav from '../components/Nav'
+import './home.css'
+
+export default function Home() {
+
+    return(
+        <>
+        <Nav></Nav>
+        <div className='home'>
+
+        </div>
+        </>
+    )
+    
+}
